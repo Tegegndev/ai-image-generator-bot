c7145fab7f41eea8b27f8d2719f9b5cf85263c2c
tegegn <tegegnw1997@gmail.com>
2025-05-22 23:02:08 -1200

    added help command

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import telebot
 from config import *
+import ssl
+import warnings
+
+
 
 bot = telebot.TeleBot(BOT_TOKEN,colorful_logs=True,)
 
 
 @bot.message_handler(commands=['start'])
-def start(messgae):
-    bot.send_message(messgae.chat.id, 'Hello, I am a bot!')
+def start(message):
+    bot.reply_to(message, 'Hello, I am a bot! built by tegegn w w')
 
 @bot.message_handler(commands=['help'])
 def help(message):
     bot.send_message(message.chat_id, 'help here')
 
-bot.polling(none_stop=True,restart_on_change=True)    
+if __name__ == '__main__':
+    print("Bot is restarted ...")
+    bot.polling(none_stop=True,restart_on_change=True,timeout=40)    
